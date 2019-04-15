30c59890f9ffe1698e6a78f848ab7ceec0ffc6c5
nerd553 <geomelkon@csd.auth.gr>
2019-04-16 00:08:46 +0300

    Study page get endpoint

--- flaskr/endpoints/StudyResource.py
+++ flaskr/endpoints/StudyResource.py
@@ -1,19 +1,19 @@
 from flask import request, jsonify
 from flask_restful import Resource
 import json, datetime
-from collections import namedtuple
 
 
 class StudyResource(Resource):
 
     def get(self):
+        if request.args.get('id'):
+            return jsonify(study=
+                           get_study(request.args.get('id')))
+
         return jsonify(studies=load_studies())
 
     def post(self):
         req = request.json
-        # Parse JSON into an object with attributes corresponding to dict keys.
-        # req = json.loads(req, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
-        # req = json.loads(req)
         args = request.args
 
         if args.get('title'):
@@ -72,3 +72,60 @@ def load_studies():
 
 def check_title(title):
     return False
+
+
+def get_study(id: int):
+    date = datetime.datetime.now().isoformat()
+    return {
+        'id': id,
+        'title': 'Title',
+        'isLive': True,
+        'launched': date,
+        'participants': {
+            'completion': '50%',
+            'total': 99,
+            'completed': 49,
+            'data': [
+                ['#1', '00:05:43', '100%', '6'],
+                ['#2', '00:05:43', '100%', '6'],
+                ['#3', '00:05:43', '100%', '6'],
+                ['#4', '00:05:43', '100%', '6'],
+                ['#5', '00:05:43', '100%', '6'],
+                ['#6', '00:05:43', '100%', '6'],
+                ['#7', '00:05:43', '100%', '6'],
+                ['#8', '00:05:43', '100%', '6'],
+                ['#9', '00:05:43', '100%', '6'],
+                ['#10', '00:05:43', '100%', '6'],
+                ['#11', '00:05:43', '100%', '6'],
+            ],
+        },
+        'cards': {
+            'average': '60%',
+            'total': 100,
+            'sorted': 60,
+            'data': [
+                ['Card1', 4, 'Category1', 2],
+                ['Card2', 4, 'Category1', 2],
+                ['Card3', 4, 'Category1', 2],
+                ['Card4', 4, 'Category1', 2],
+                ['Card5', 4, 'Category1', 2],
+                ['Card6', 4, 'Category1', 2],
+                ['Card7', 4, 'Category1', 2],
+            ],
+        },
+        'categories': {
+            'similarity': '20%',
+            'total': 20,
+            'similar': 100,
+            'merged': 2,
+            'data': [
+                ['Category1', 3, 'Card1', 4, 4],
+                ['Category2', 3, 'Card1', 4, 4],
+                ['Category3', 3, 'Card1', 4, 4],
+                ['Category4', 3, 'Card1', 4, 4],
+                ['Category5', 3, 'Card1', 4, 4],
+                ['Category6', 3, 'Card1', 4, 4],
+                ['Category7', 3, 'Card1', 4, 4],
+            ],
+        }
+    }
