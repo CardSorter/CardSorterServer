48a0cb240da42a4130cad5bbe5457f63c2a0e6d2
nerd553 <geomelkon@csd.auth.gr>
2019-04-19 20:17:23 +0300

    Moved file serving to wsgi

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,50 +3,35 @@ import os
 from flask import Flask
 from flask_cors import CORS
 
-from flaskr import db
-from .blueprints.designer import designer
-from .blueprints.auth import auth
-from .blueprints.cardSorter import card_sorter
 from flaskr.InitializeEndpoints import InitializeEndpoints
 
 
-def create_app(test_config=None):
+test_config = None
 
-    # Configuration
-    app = Flask(__name__, instance_relative_config=True, static_folder='./public/card_sorter/',
-                template_folder="./public/card_sorter/")
-    app.config.from_mapping(
-        SECRET_KEY='development',
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    CORS(app)
+# Configuration
+app = Flask(__name__, instance_relative_config=True, static_folder='./public/card_sorter/',
+            template_folder="./public/card_sorter/")
+app.config.from_mapping(
+    SECRET_KEY='development',
+    # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
+CORS(app)
 
-    if test_config is None:
-        app.config.from_pyfile('Config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+if test_config is None:
+    app.config.from_pyfile('Config.py', silent=True)
+else:
+    app.config.from_mapping(test_config)
 
-    # Ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# Ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # First level API
-
-    # Route: /auth
-    app.register_blueprint(auth)
-
-    # Route: /sort
-    app.register_blueprint(card_sorter)
-
-    # Route: /
-    app.register_blueprint(designer)
-
-    # Endpoints
-    InitializeEndpoints(app)
-
-    return app
+# First level API
 
 
-create_app().run()
+# Endpoints
+InitializeEndpoints(app)
+
+app.run()
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='card sorter',
+    version='0.1',
+    packages=['flaskr'],
+    url='http://83.212.97.237/',
+    license='',
+    author='George Melissourgos',
+    author_email='',
+    description=''
+)
