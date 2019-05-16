6f039bd3d57cea0409994d0b49f52cefe624c5f2
nerd553 <geomelkon@csd.auth.gr>
2019-05-16 18:48:19 +0300

    Surpressed register

--- flaskr/endpoints/UserResource.py
+++ flaskr/endpoints/UserResource.py
@@ -12,19 +12,20 @@ class UserResource(Resource):
     def post(self):
 
         if len(request.args) > 0 and request.args['register']:
-            username = request.json.get('username')
-            password = request.json.get('password')
-            email = request.json.get('email')
-            user = User()
-            error_msg = user.create_user(username, password, email)
-
-            if error_msg:
-                return jsonify(error=error_msg)
-
-            resp = make_response(jsonify({
-                'location': Config.url,
-                'auth_token':  user.auth_token.decode()
-            }))
+            pass
+            # username = request.json.get('username')
+            # password = request.json.get('password')
+            # email = request.json.get('email')
+            # user = User()
+            # error_msg = user.create_user(username, password, email)
+            #
+            # if error_msg:
+            #     return jsonify(error=error_msg)
+            #
+            # resp = make_response(jsonify({
+            #     'location': Config.url,
+            #     'auth_token':  user.auth_token.decode()
+            # }))
 
         else:  # Login
             username = request.json.get('username')
