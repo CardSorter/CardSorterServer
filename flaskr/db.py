9b1e9da8afd735056241014f74f1396cb41f0263
George Melissourgos <george.melissourgos@callista.ee>
2020-12-22 17:03:14 +0200

    Updated dev environment

--- flaskr/db.py
+++ flaskr/db.py
@@ -7,12 +7,18 @@ import os
 def get_db():
     if 'db' not in g:
         db_username = os.environ.get('MONGODB_USERNAME', 'rest_api')
-        db_password = os.environ.get('MONGODB_PASSWORD', 'example')
+        db_password = os.environ.get('MONGODB_PASSWORD', 'password')
         db_host = os.environ.get('MONGODB_HOSTNAME', '127.0.0.1')
         db_database = os.environ.get('MONGODB_DATABASE', 'card_sorter')
-        mongoURI = 'mongodb://' + db_username + ':' + db_password + '@' + db_host + ':27017/' + db_database
 
-        conn = MongoClient(mongoURI, authSource=str(db_database))
+        if (os.environ.get('MONGODB_USERNAME')):
+            mongoURI = 'mongodb://' + db_username + ':' + db_password + '@' + db_host + ':27017/' + db_database
+            conn = MongoClient(mongoURI, authSource=str(db_database))
+        else:
+            # Do not authenticate over a db for development
+            mongoURI = 'mongodb://' + db_username + ':' + db_password + '@' + db_host + ':27017/'
+            conn = MongoClient(mongoURI)
+            
         g.conn = conn
         g.db = conn[db_database]
 
---
+++ setup_db.sh
@@ -0,0 +1,3 @@
+docker exec -it card_sorter_mongodbdev bash -c "mongo -u rest_api -p password & use card_sorter"
+
+exit
